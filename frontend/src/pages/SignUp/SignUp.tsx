30a6aafc4e6791bc93012c33e3f878784cdd671e
unknown <johansantanagalvan.job@gmail.com>
2022-11-23 00:49:48 +0000

    starting with validations

--- frontend/src/pages/SignUp/SignUp.tsx
+++ frontend/src/pages/SignUp/SignUp.tsx
@@ -1,10 +1,14 @@
 import './SignUp.css';
-
+import { Link } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
+import { useForm } from "react-hook-form";
+
 
 export const SignUp = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = (data : any) => console.log(data);
     return (
         <>
             <Container>
@@ -12,12 +16,16 @@ export const SignUp = () => {
                     <Col xs={8} md={6}>
                         <div className="sign-up-style">
                             <h2 className="sign-up-title">Sign Up</h2>
-                            <form>
+                            <form onSubmit={handleSubmit(onSubmit)}>
                                 <div className='sign-up-field'>
-                                    <input type="text" placeholder='Write your first name' required></input>
+                                    <input {...register("firstName",{required:true, maxLength: 20})} 
+                                     aria-invalid={errors.firstName ? "true" : "false"} placeholder='Write your first name'/>
+                                     
+                                     {errors.firstName?.type === 'required' && <p role="alert">First name is required</p>}
                                 </div>
+
                                 <div className='sign-up-field'>
-                                    <input type="text" placeholder='Write your last name' required></input>
+                                    <input {...register("lastName",{required:true, maxLength: 20,pattern: /^[A-Za-z]+$/i })} placeholder='Write your last name' required></input>
                                 </div>
                                 <div className='sign-up-field'>
                                     <input type="text" placeholder='Write your email' required></input>
@@ -28,11 +36,14 @@ export const SignUp = () => {
                                 <div className='sign-up-field'>
                                     <input type="date" placeholder='Write your date of birth' required></input>
                                 </div>
+                                <input type="submit"/>
                                 <button className="sign-up-button">Sign Up</button>
                             </form>
                             <div className='sign-up-extra'>
                                 <h2 className='sign-up-subtext'>Already Logged?</h2>
+                                <Link to="/LogIn">
                                 <button className="sign-up-button">Log In</button>
+                                </Link>
                             </div>
                         </div>
                     </Col>
