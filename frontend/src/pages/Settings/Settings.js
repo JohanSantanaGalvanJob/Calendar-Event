68fa16d612998bc1b051464720e51a11609f92b6
Kevin Rodríguez <kevinrodriguezvalentin@gmail.com>
2023-03-01 16:12:37 +0000

    Settings page buttons updated
    Buttons enhanced to ease its usability and accesibility when using just a keyboard

--- frontend/src/pages/Settings/Settings.js
+++ frontend/src/pages/Settings/Settings.js
@@ -15,15 +15,6 @@ const Settings = () => {
         window.location.replace('https://localhost:443/helpSystem/Bienvenida.html');
     }
 
-    const handleHelpClick = () => {
-        loadHelpSystem();
-    };
-
-    const handleClick = (event) => {
-        console.log(event.target)
-        // navigate(url);
-    };
-
     return (
         <>
             <Navbar></Navbar>
@@ -34,60 +25,51 @@ const Settings = () => {
 
                     <>
 
-                        {/* <Link to='/ChangePassword'> */}
-                            <div onClick={() => { navigate("/ChangePassword") }} className="settings-field">
-                                <div>
-                                    <img src="./icons/Ayuda/llave.png"></img>
-                                </div>
-                                <div className='settings-content'>
-                                    <h2 className='settings-subtitle'>Change Password</h2>
-                                    <p className="settings-text">Change your password at any moment</p>
-                                    <div className="settings-line"></div>
-                                </div>
+                        <Link to="/ChangePassword" className="settings-field">
+                            <div>
+                                <img src="./icons/Ayuda/llave.png"></img>
                             </div>
-                        {/* </Link> */}
-
-                        {/* <Link to='/ChangeView'> */}
-                            <div onClick={() => { navigate("/Changeview") }} className="settings-field">
-                                <div>
-                                    <img src="./icons/Ayuda/paleta.png"></img>
-                                </div>
-                                <div className='settings-content'>
-                                    <h2 className='settings-subtitle'>Change View</h2>
-                                    <p className="settings-text">Change the palette of our app</p>
-                                    <div className="settings-line"></div>
-                                </div>
+                            <div className='settings-content'>
+                                <h2 className='settings-subtitle'>Change Password</h2>
+                                <p className="settings-text">Change your password at any moment</p>
+                                <div className="settings-line"></div>
                             </div>
-                        {/* </Link> */}
+                        </Link>
 
-                        {/* <Link to='/ChangeLanguage'> */}
-                            <div onClick={() => { navigate("/ChangeLanguage") }} className="settings-field">
-                                <div>
-                                    <img src="./icons/Ayuda/mundo.png"></img>
-                                </div>
-                                <div className='settings-content'>
-                                    <h2 className='settings-subtitle'>Change Language</h2>
-                                    <p className="settings-text">Change the palette of our app</p>
-                                    <div className="settings-line"></div>
-                                </div>
+                        <Link to='/ChangeView' className="settings-field">
+                            <div>
+                                <img src="./icons/Ayuda/paleta.png"></img>
                             </div>
-                        {/* </Link> */}
-
-                        {/* <Link to='/DeleteAccount'> */}
-                            <div onClick={() => { navigate("/DeleteAccount") }} className="settings-field">
-                                <div>
-                                    <img src="./icons/Ayuda/basura.png"></img>,
-                                </div>
-                                <div className='settings-content'>
-                                    <h2 className='settings-subtitle'>Delete your Account</h2>
-                                    <p className="settings-text">Delete your current account</p>
-                                    <div className="settings-line"></div>
-                                </div>
+                            <div className='settings-content'>
+                                <h2 className='settings-subtitle'>Change View</h2>
+                                <p className="settings-text">Change the palette of our app</p>
+                                <div className="settings-line"></div>
                             </div>
-                        {/* </Link> */}
+                        </Link>
 
-                        {/* <Link to='/EmailSent'> */}
-                        <div onClick={() => { navigate("/EmailSent") }} className="settings-field">
+                        <Link to='/ChangeLanguage' className="settings-field">
+                            <div>
+                                <img src="./icons/Ayuda/mundo.png"></img>
+                            </div>
+                            <div className='settings-content'>
+                                <h2 className='settings-subtitle'>Change Language</h2>
+                                <p className="settings-text">Change the palette of our app</p>
+                                <div className="settings-line"></div>
+                            </div>
+                        </Link>
+
+                        <Link to='/DeleteAccount' className="settings-field">
+                            <div>
+                                <img src="./icons/Ayuda/basura.png"></img>,
+                            </div>
+                            <div className='settings-content'>
+                                <h2 className='settings-subtitle'>Delete your Account</h2>
+                                <p className="settings-text">Delete your current account</p>
+                                <div className="settings-line"></div>
+                            </div>
+                        </Link>
+
+                        <Link to='/EmailSent' className="settings-field">
                             <div>
                                 <img src="/icons/Ayuda/sobre.png"></img>
                             </div>
@@ -96,11 +78,10 @@ const Settings = () => {
                                 <p className="settings-text">Control if you want to receive notifications from us or not.</p>
                                 <div className="settings-line"></div>
                             </div>
-                        </div>
-                        {/* </Link> */}
+                        </Link>
 
 
-                        <div className="settings-field" onClick={loadHelpSystem}>
+                        <Link onClick={loadHelpSystem} className="settings-field">
                             <div>
                                 <img src="/icons/Ayuda/interrogatorio.png"></img>
                             </div>
@@ -109,7 +90,7 @@ const Settings = () => {
                                 <p className="settings-text">See how to navigate and perform all actions</p>
                                 <div className="settings-line"></div>
                             </div>
-                        </div>
+                        </Link>
 
 
                     </>
@@ -117,42 +98,36 @@ const Settings = () => {
 
                 ) :
                     <>
-                        {/* <Link to='/ChangeView'> */}
-                            <div onClick={() => { navigate("/ChangeView") }} className="settings-field">
-                                <div>
-                                    <img src="/icons/Ayuda/paleta.png"></img>
-                                </div>
-                                <div className='settings-content'>
-                                    <h2 className='settings-subtitle'>Change View</h2>
-                                    <p className="settings-text">Change the palette of our app</p>
-                                    <div className="settings-line"></div>
-                                </div>
+                        <Link to='/ChangeView' className="settings-field">
+                            <div>
+                                <img src="/icons/Ayuda/paleta.png"></img>
                             </div>
-                        {/* </Link> */}
-
-                        {/* <Link to='/ChangeLanguage'> */}
-                            <div onClick={() => { navigate("/ChangeLanguage") }} className="settings-field">
-                                <div>
-                                    <img src="/icons/Ayuda/mundo.png"></img>
-                                </div>
-                                <div className='settings-content'>
-                                    <h2 className='settings-subtitle'>Change Language</h2>
-                                    <p className="settings-text">Change the palette of our app</p>
-                                    <div className="settings-line"></div>
-                                </div>
+                            <div className='settings-content'>
+                                <h2 className='settings-subtitle'>Change View</h2>
+                                <p className="settings-text">Change the palette of our app</p>
+                                <div className="settings-line"></div>
                             </div>
-                        {/* </Link> */}
+                        </Link>
 
-                        <Link to='/helpSystem/bienvenida.html'>
-                            <div className="settings-field">
-                                <div>
-                                    <img src="/icons/Ayuda/interrogatorio.png"></img>
-                                </div>
-                                <div className='settings-content'>
-                                    <h2 className='settings-subtitle'>Help</h2>
-                                    <p className="settings-text">See how to navigate and perform all actions</p>
-                                    <div className="settings-line"></div>
-                                </div>
+                        <Link to='/ChangeLanguage' className="settings-field">
+                            <div>
+                                <img src="/icons/Ayuda/mundo.png"></img>
+                            </div>
+                            <div className='settings-content'>
+                                <h2 className='settings-subtitle'>Change Language</h2>
+                                <p className="settings-text">Change the palette of our app</p>
+                                <div className="settings-line"></div>
+                            </div>
+                        </Link>
+
+                        <Link onClick={loadHelpSystem} className="settings-field">
+                            <div>
+                                <img src="/icons/Ayuda/interrogatorio.png"></img>
+                            </div>
+                            <div className='settings-content'>
+                                <h2 className='settings-subtitle'>Help</h2>
+                                <p className="settings-text">See how to navigate and perform all actions</p>
+                                <div className="settings-line"></div>
                             </div>
                         </Link>
 
